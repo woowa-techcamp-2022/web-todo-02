b6a0e866735f92b8686c842653cfc0dd5ea7d33b
kms-97 <msg016@naver.com>
2022-07-12 09:57:42 +0900

    feat: card 컴포넌트 추가

---
+++ src/js/components/card/Card.js
@@ -0,0 +1,47 @@
+import Header from './header/Header.js';
+import Content from './content/Content.js';
+import Footer from './footer/Footer.js';
+
+export default class {
+  constructor($target, title, content, author) {
+    this.$target = $target;
+    this.$element = document.createElement('li');
+    this.state = {
+      title,
+      content,
+      author,
+    };
+
+    this.$header = new Header(
+      this.$element,
+      this.state.title,
+      this.deleteCard.bind(this)
+    );
+    this.$content = new Content(this.$element, this.state.content);
+    this.$footer = new Footer(this.$element, this.state.author);
+
+    this.$element.classList.add('card');
+    this.$target.appendChild(this.$element);
+  }
+
+  setState(newState) {
+    this.state = {
+      ...this.state,
+      ...newState,
+    };
+  }
+
+  deleteCard() {
+    this.$element.remove();
+  }
+
+  changeTitle(title) {
+    this.setState({ title });
+    this.$header.changeTitle(title);
+  }
+
+  changeContent(content) {
+    this.setState({ content });
+    this.$content.setState(content);
+  }
+}
