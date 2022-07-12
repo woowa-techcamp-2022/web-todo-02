1cab8ca0ef0824d5ad5d9015a77ff25d6009a25f
kms-97 <msg016@naver.com>
2022-07-12 10:55:25 +0900

    feat: 삭제버튼 마우스 올리면 카드 클래스 추가

--- src/js/components/button/DeleteBtn.js
+++ src/js/components/button/DeleteBtn.js
@@ -1,16 +1,32 @@
 export default class {
-    constructor($target, className, onclick) {
-        this.$target = $target;
-        this.$element = document.createElement('button');
+  constructor($target, className, onclick) {
+    this.$target = $target;
+    this.$element = document.createElement('button');
 
-        this.$element.classList.add(className);
-        this.$element.addEventListener('click', onclick);
-        this.$element.innerHTML = `
+    this.$element.classList.add(className);
+    this.$element.addEventListener('click', onclick);
+    this.$element.addEventListener('mouseenter', this.onMouseEnter);
+    this.$element.addEventListener('mouseleave', this.onMouseLeave);
+    this.$element.innerHTML = `
         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M7.5 17.25L6.75 16.5L11.25 12L6.75 7.5L7.5 6.75L12 11.25L16.5 6.75L17.25 7.5L12.75 12L17.25 16.5L16.5 17.25L12 12.75L7.5 17.25Z" fill="black"/>
         </svg>
-        `
+        `;
 
-        this.$target.appendChild(this.$element);
-    }
+    this.$target.appendChild(this.$element);
+  }
+
+  onMouseEnter(e) {
+    const card = e.target.closest('.card');
+    if (!card) return;
+
+    card.classList.add('danger');
+  }
+
+  onMouseLeave(e) {
+    const card = e.target.closest('.card');
+    if (!card) return;
+
+    card.classList.remove('danger');
+  }
 }
