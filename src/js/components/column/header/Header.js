9dfa2d25db7d3fc7267c6644e3028eed4e58d67e
kms-97 <msg016@naver.com>
2022-07-12 10:01:38 +0900

    fix: import 경로 오류 수정

--- src/js/components/column/header/Header.js
+++ src/js/components/column/header/Header.js
@@ -1,15 +1,19 @@
-import TitleContainer from "./TitleContainer.js";
-import ButtonContainer from "./ButtonContainer.js";
+import TitleContainer from './titleContainer/TitleContainer.js';
+import ButtonContainer from './btnContainer/ButtonContainer.js';
 
 export default class {
-    constructor($target, title, addCard, deleteColumn) {
-        this.$target = $target;
-        this.$element = document.createElement('div');
+  constructor($target, title, addCard, deleteColumn) {
+    this.$target = $target;
+    this.$element = document.createElement('div');
 
-        this.$titleContainer = new TitleContainer(this.$element, title);
-        this.$btnContainer = new ButtonContainer(this.$element, addCard, deleteColumn);
+    this.$titleContainer = new TitleContainer(this.$element, title);
+    this.$btnContainer = new ButtonContainer(
+      this.$element,
+      addCard,
+      deleteColumn
+    );
 
-        this.$element.classList.add('column-header');
-        this.$target.appendChild(this.$element);
-    }
+    this.$element.classList.add('column-header');
+    this.$target.appendChild(this.$element);
+  }
 }
