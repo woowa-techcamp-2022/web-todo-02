8cedd07ad7144b94c6cc07e64f6204bbb56d9d55
woochul2 <leewoo0686@gmail.com>
2022-07-13 22:43:25 +0900

    feat: 카드 위아래로 이동하는 기능 구현

--- src/js/components/Card.js
+++ src/js/components/Card.js
@@ -50,20 +50,50 @@ export default class {
     this.$element.style.left = `${rect.left}px`;
   }
 
-  replaceCardWithSkeleton() {
+  addSkeleton() {
     const $skeleton = this.$element.cloneNode(true);
     $skeleton.classList.add('skeleton');
     this.$element.parentNode.insertBefore($skeleton, this.$element);
+  }
+
+  getAllCardsWithoutMovingCard() {
+    return document.querySelectorAll('.card:not(.moving):not(.skeleton)');
+  }
+
+  replaceCardWithSkeleton(event) {
+    const $originalCard = event.target;
+    const $skeleton = document.querySelector('.skeleton');
+    $skeleton.classList.add('replaced');
+    event.target.replaceWith($skeleton.cloneNode(true));
+    $skeleton.replaceWith($originalCard);
+  }
+
+  setMouseEnterEventToAllCards() {
+    this.getAllCardsWithoutMovingCard().forEach((card) => {
+      card.onmouseenter = this.replaceCardWithSkeleton.bind(this);
+    });
+  }
+
+  removeMouseEnterEventToAllCards() {
+    this.getAllCardsWithoutMovingCard().forEach((card) => {
+      card.onmouseenter = null;
+    });
   }
 
   dragStart(event) {
     if (event.target.closest('.card-header-delete')) return;
 
     this.setCardInitialPosition();
-    this.replaceCardWithSkeleton();
+    this.addSkeleton();
     this.$element.classList.add('moving');
+    // 1. pointer-events를 none으로 설정한 이유: 카드 드래그 시 다른 카드의 mouseenter 이벤트를 감지하기 위해
+    // 2. setTimeout으로 delay를 넣어준 이유: dblclick 이벤트를 감지하기 위해
+    const styleTimeoutID = setTimeout(() => {
+      this.$element.style.pointerEvents = 'none';
+    }, 100);
 
     const dragStartPosition = { x: event.clientX, y: event.clientY };
+    this.setMouseEnterEventToAllCards();
     const dragEvent = (event) => {
       this.drag(event, dragStartPosition);
     };
@@ -71,6 +101,8 @@ export default class {
     window.onmouseup = (event) => {
       this.dragEnd(event);
       window.removeEventListener('mousemove', dragEvent);
+      clearTimeout(styleTimeoutID);
+      this.$element.style.pointerEvents = '';
       window.onmouseup = null;
     };
   }
@@ -83,8 +115,15 @@ export default class {
 
   dragEnd() {
     this.$element.removeAttribute('style');
-    document.querySelector('.skeleton').remove();
+    const $skeleton = document.querySelector('.skeleton');
+    // dbl 클릭 이벤트를 감지하기 위해 잔상 카드가 이동했을 때만 엘리먼트와 교체해주도록 함
+    if ($skeleton.classList.contains('replaced')) {
+      $skeleton.replaceWith(this.$element);
+    } else {
+      $skeleton.remove();
+    }
     this.$element.classList.remove('moving');
+    this.removeMouseEnterEventToAllCards();
   }
 
   getTitle() {
