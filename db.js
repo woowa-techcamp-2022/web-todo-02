d5858f6c06ad13540f6560348ca10d3a8ce05728
kms-97 <msg016@naver.com>
2022-07-13 17:23:09 +0900

    feat: db 커넥트 promise로 변경

--- db.js
+++ db.js
@@ -1,11 +1,15 @@
-import mysql from 'mysql2';
+import mysql from 'mysql2/promise';
 
-const connection = mysql.createConnection({
+const options = {
   host: 'localhost',
   user: 'root',
   password: '',
   database: 'baemin_todo',
-});
+};
+
+function getConnection() {
+  return mysql.createConnection(options);
+}
 
 // 초기 테스트용 임시 함수. 나중에 지워도 됨.
-export default connection;
+export default getConnection;
